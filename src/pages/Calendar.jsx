d8aa25dcf83e2a3570dfc3b8f86387c1d5bf38a9
vickry <vickrykamaluddin2119@gmail.com>
2023-09-12 16:12:11 +0700

    calender2

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import dayGridPlugin from '@fullcalendar/daygrid' 
+import timeGridPlugin from '@fullcalendar/timegrid'
 
 const Calendar = () => {
-  
+ 
     return (
-        <div className="border-2 py-4 px-4 my-64 mx-64   bg-white shadow-md rounded-lg m-2 text-sm ">
-          <FullCalendar 
-            plugins={[ dayGridPlugin ]}
-            initialView="dayGridMonth"
-          />
+        <div className="border-2 py-4 px-4 bg-white shadow-md rounded-lg m-2 text-sm ">
+         <FullCalendar
+      plugins={[dayGridPlugin, timeGridPlugin]}
+      initialView="dayGridMonth"
+      selectable={true}
+      headerToolbar={{
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay',
+      }}
+      dateClick={(info) => {
+        alert('clicked ' + info.dateStr);
+      }}
+      select={(info) => {
+        alert(
+          'selected ' + info.startStr + ' to ' + info.endStr
+        );
+      }}
+    />
       </div>
     )
 }
